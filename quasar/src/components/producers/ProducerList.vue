85a53f197cee17e7659fa6a3f9849aff18b22093
Dmitrii Sivkov <sivkovdw@yandex.ru>
2022-05-10 18:41:12 +0300

    producerList order counter

--- quasar/src/components/producers/ProducerList.vue
+++ quasar/src/components/producers/ProducerList.vue
@@ -12,12 +12,16 @@
 			<template v-slot:item="props">
 				<div class="q-table__grid-item col-xs-12 col-sm-6 col-md-4 col-lg-3">
 					<div
+						:class="{ 'bg-light-green-2': cart.hasOwnProperty(props.row.id) }"
 						class="q-table__grid-item-card q-table__card cursor-pointer"
 						@click="show(props.row.id)"
 					>
 						<div class="q-table__grid-item-row">
 							<div class="q-table__grid-item-value">
 								{{ props.row.title }}
+								<span v-if="cart.hasOwnProperty(props.row.id)">
+									{{ "(" + Object.keys(cart[props.row.id]).length + ")" }}
+								</span>
 							</div>
 						</div>
 					</div>
