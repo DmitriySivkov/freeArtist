762afa1e36fe0152cb85bec3c7bcf480ab6b1d8a
Dmitrii Sivkov <sivkovdw@yandex.ru>
2022-09-19 22:12:27 +0300

    new image func part 1

---
+++ quasar/src/components/dialogs/AddImageDialog.vue
@@ -0,0 +1,59 @@
+<template>
+	<q-dialog
+		ref="dialogRef"
+		@hide="onDialogHide"
+	>
+		<q-card class="q-dialog-plugin">
+			<q-card-section
+				horizontal
+				class="q-col-gutter-sm q-pa-md"
+			>
+				<div class="col-6">
+					<q-btn
+						color="primary"
+						label="Открыть галерею"
+						class="q-pa-lg full-width"
+						@click="test(1)"
+					/>
+				</div>
+				<div class="col-6">
+					<q-btn
+						color="primary"
+						label="Сделать снимок"
+						class="q-pa-lg full-width"
+						@click="test(2)"
+					/>
+				</div>
+			</q-card-section>
+		</q-card>
+	</q-dialog>
+</template>
+
+<script>
+import { useDialogPluginComponent } from "quasar"
+
+export default {
+	props: {},
+
+	emits: [
+		...useDialogPluginComponent.emits,
+		"zxc"
+	],
+
+	setup (props, {emit}) {
+		const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } = useDialogPluginComponent()
+		// abuse "onOk" event
+		const test = (option) => {
+			onDialogOK(option)
+		}
+		return {
+			// This is REQUIRED;
+			// Need to inject these (from useDialogPluginComponent() call)
+			// into the vue scope for the vue html template
+			dialogRef,
+			onDialogHide,
+			test
+		}
+	}
+}
+</script>
--- quasar/src/components/producers/producerProductSettingsTabs/ProducerProductSettingImagesTab.vue
+++ quasar/src/components/producers/producerProductSettingsTabs/ProducerProductSettingImagesTab.vue
@@ -1,14 +1,23 @@
 <template>
-	123
+	<q-btn
+		flat
+		label="Добавить изображение"
+		class="bg-primary text-white full-width q-pa-lg"
+		@click="showFilePrompt"
+	/>
 </template>
 
 <script>
+import { useQuasar } from "quasar"
 import { useRouter } from "vue-router"
 import { useNotification } from "src/composables/notification"
 import { ref, computed } from "vue"
 import { useStore } from "vuex"
 import cordovaCamera from "src/services/cordova-camera"
+import AddImageDialog from "src/components/dialogs/AddImageDialog"
 export default {
+	// eslint-disable-next-line vue/no-unused-components
+	components: { AddImageDialog },
 	props: {
 		selectedProduct: {
 			type: Object,
@@ -16,13 +25,23 @@ export default {
 		}
 	},
 	setup(props) {
-		const img = ref()
-		const uploadImageFromCamera = async() => {
+		const $q = useQuasar()
+		const showFilePrompt = () => {
+			$q.dialog({
+				component: AddImageDialog
+			}).onOk((option) => {
+				alert(option)
+			})
+		}
+
+		const img = ref(null)
+		const takePicture = async() => {
 			const base64 = await cordovaCamera.getBase64FromCamera()
 		}
 
 		return {
-
+			takePicture,
+			showFilePrompt
 		}
 	}
 }
