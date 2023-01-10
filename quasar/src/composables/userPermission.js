cfc1731f6b896dfee86295aa74aa33b9fdf8684d
Dmitrii Sivkov <sivkovdw@yandex.ru>
2023-01-10 20:15:40 +0300

    permission check fix

--- quasar/src/composables/userPermission.js
+++ quasar/src/composables/userPermission.js
@@ -6,15 +6,18 @@ export const useUserPermission = () => {
 	const user = computed(() => $store.state.user)
 	const team_store = computed(() => $store.state.team)
 
-	const hasPermission = (team_id, permission_name) =>
-		user.value.data
-			.permissions
-			.filter((p) => p.pivot.team_id === team_id)
+	const hasPermission = (team_id, permission_name) => {
+		const team = team_store.value.user_teams.find((t) => t.id === team_id)
+
+		const team_user = team.hasOwnProperty("users") ?
+			team.users.find((u) => u.id === user.value.data.id) : false
+
+		return user.value.data.permissions.filter((p) => p.pivot.team_id === team_id)
 			.map((p) => p.name)
 			.includes(permission_name) ||
-		team_store.value.user_teams
-			.find((t) => t.id === team_id)
-			.user_id === user.value.data.id
+		team.user_id === user.value.data.id ||
+		(team_user ? team_user.permissions.map((p) => p.name).includes(permission_name) : false)
+	}
 
 	return { hasPermission }
 }
