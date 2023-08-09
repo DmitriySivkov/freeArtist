25d9918a2cf503bb0f9e72c40ac4b8bc9b6d2942
Dmitrii Sivkov <sivkovdw@yandex.ru>
2023-08-09 21:40:13 +0300

    images tab: rework as script setup

--- quasar/src/components/producers/producerProductSettingsTabs/ProducerProductSettingImagesTab.vue
+++ quasar/src/components/producers/producerProductSettingsTabs/ProducerProductSettingImagesTab.vue
@@ -114,172 +114,155 @@
 	/>
 </template>
 
-<script>
+<script setup>
 import { useQuasar } from "quasar"
 import { ref } from "vue"
 import { Plugins, CameraResultType } from "@capacitor/core"
 import { cameraService } from "src/services/cameraService"
 import AddImageDialog from "src/components/dialogs/AddImageDialog.vue"
 import _ from "lodash"
-export default {
-	components: {
-		// eslint-disable-next-line vue/no-unused-components
-		AddImageDialog
-	},
-	props: {
-		modelValue: {
-			type: Object,
-			default: () => ({})
-		}
-	},
-	setup(props, { emit }) {
-		const $q = useQuasar()
-		const image = ref(null)
 
-		const uploader = ref(null)
-		const is_dragging = ref(false)
-		const is_loading = ref(false)
-
-		const { base64ToBlob } = cameraService()
-		const { Camera } = Plugins
-
-		const backend_server = process.env.BACKEND_SERVER
-
-		const showFilePrompt = () => {
-			if ($q.platform.is.desktop) {
-				fromGallery()
-				return
-			}
-
-			$q.dialog({
-				component: AddImageDialog
-			}).onOk((option) => {
-				if (option === 1)
-					fromGallery()
-				if (option === 2)
-					fromCamera()
-			})
-		}
-
-		const fromGallery = () => {
-			uploader.value.pickFiles()
-		}
-
-		// todo - check camera
-		const fromCamera = async () => {
-			const img = await Camera.getPhoto({
-				quality: 90,
-				allowEditing: false,
-				resultType: CameraResultType.DataUrl
-			})
-			const blob = await base64ToBlob(img.dataUrl)
-			const img_file = new File([blob], "no-matter.jpg")
-			uploader.value.addFiles([img_file])
-		}
-
-		const imageCommitted = (files) => {
-			const tmp_images = files.map((f) => {
-				return {
-					key: f.__key,
-					src: URL.createObjectURL(f),
-					instance: f
-				}
-			})
-
-			if (!props.modelValue.committed_images) {
-				emit("update:modelValue", {...props.modelValue, committed_images: tmp_images })
-				return
-			}
-
-			emit("update:modelValue", {
-				...props.modelValue,
-				committed_images: [...tmp_images, ...props.modelValue.committed_images]
-			})
-
-		}
-
-		const removeCommittedImage = (committed_image_key) => {
-			uploader.value.removeFile(
-				props.modelValue.committed_images.find((i) => i.key === committed_image_key).instance
-			)
-
-			// if deleting the last committed image
-			if (props.modelValue.committed_images.length === 1) {
-				emit(
-					"update:modelValue",
-					{
-						...Object.keys(props.modelValue)
-							.filter((prop) => prop !== "committed_images")
-							.reduce((carry, prop) => {
-								carry[prop] = props.modelValue[prop]
-								return carry
-							}, {})
-					}
-				)
-				return
-			}
-
-			emit("update:modelValue", {
-				...props.modelValue,
-				committed_images: props.modelValue.committed_images.filter((i) => i.key !== committed_image_key)
-			})
-		}
-
-		const toggleImageRemoval = (image_id) => {
-			let images = _.clone(props.modelValue.images)
-			let image_index = images.findIndex((i) => i.id === image_id)
-			let image = images.find((i) => i.id === image_id)
-
-			if (image.hasOwnProperty("to_delete")) {
-				delete image.to_delete
-			} else {
-				image.to_delete = 1
-			}
-
-			images[image_index] = image
-
-			emit("update:modelValue", {
-				...props.modelValue,
-				images
-			})
-		}
-
-		const toggleImageThumbnail = (image_id) => {
-			let thumbnail_id = props.modelValue.thumbnail_id === image_id ? null : image_id
-
-			emit("update:modelValue", Object.assign(props.modelValue, { thumbnail_id }))
-		}
-
-		// todo rejection handling
-		const imageRejected = (rejected_entries) => {
-			console.log(rejected_entries)
-		}
-
-		// todo - bind uploading to this method
-		const upload = (files) => {
-
-		}
-
-		const drop = (e) => {
-			is_dragging.value = false
-			uploader.value.addFiles([e.dataTransfer.files[0]])
-		}
-
-		return {
-			image,
-			uploader,
-			showFilePrompt,
-			imageCommitted,
-			backend_server,
-			is_dragging,
-			is_loading,
-			drop,
-			removeCommittedImage,
-			imageRejected,
-			upload,
-			toggleImageRemoval,
-			toggleImageThumbnail
-		}
+const props = defineProps({
+	modelValue: {
+		type: Object,
+		default: () => ({})
 	}
+})
+
+const emit = defineEmits([
+	"update:modelValue"
+])
+
+const $q = useQuasar()
+const image = ref(null)
+
+const uploader = ref(null)
+const is_dragging = ref(false)
+const is_loading = ref(false)
+
+const { base64ToBlob } = cameraService()
+const { Camera } = Plugins
+
+const backend_server = process.env.BACKEND_SERVER
+
+const showFilePrompt = () => {
+	if ($q.platform.is.desktop) {
+		fromGallery()
+		return
+	}
+
+	$q.dialog({
+		component: AddImageDialog
+	}).onOk((option) => {
+		if (option === 1)
+			fromGallery()
+		if (option === 2)
+			fromCamera()
+	})
 }
+
+const fromGallery = () => {
+	uploader.value.pickFiles()
+}
+
+// todo - check camera
+const fromCamera = async () => {
+	const img = await Camera.getPhoto({
+		quality: 90,
+		allowEditing: false,
+		resultType: CameraResultType.DataUrl
+	})
+	const blob = await base64ToBlob(img.dataUrl)
+	const img_file = new File([blob], "no-matter.jpg")
+	uploader.value.addFiles([img_file])
+}
+
+const imageCommitted = (files) => {
+	const tmp_images = files.map((f) => {
+		return {
+			key: f.__key,
+			src: URL.createObjectURL(f),
+			instance: f
+		}
+	})
+
+	if (!props.modelValue.committed_images) {
+		emit("update:modelValue", {...props.modelValue, committed_images: tmp_images })
+		return
+	}
+
+	emit("update:modelValue", {
+		...props.modelValue,
+		committed_images: [...tmp_images, ...props.modelValue.committed_images]
+	})
+
+}
+
+const removeCommittedImage = (committed_image_key) => {
+	uploader.value.removeFile(
+		props.modelValue.committed_images.find((i) => i.key === committed_image_key).instance
+	)
+
+	// if deleting the last committed image
+	if (props.modelValue.committed_images.length === 1) {
+		emit(
+			"update:modelValue",
+			{
+				...Object.keys(props.modelValue)
+					.filter((prop) => prop !== "committed_images")
+					.reduce((carry, prop) => {
+						carry[prop] = props.modelValue[prop]
+						return carry
+					}, {})
+			}
+		)
+		return
+	}
+
+	emit("update:modelValue", {
+		...props.modelValue,
+		committed_images: props.modelValue.committed_images.filter((i) => i.key !== committed_image_key)
+	})
+}
+
+const toggleImageRemoval = (image_id) => {
+	let images = _.clone(props.modelValue.images)
+	let image_index = images.findIndex((i) => i.id === image_id)
+	let image = images.find((i) => i.id === image_id)
+
+	if (image.hasOwnProperty("to_delete")) {
+		delete image.to_delete
+	} else {
+		image.to_delete = 1
+	}
+
+	images[image_index] = image
+
+	emit("update:modelValue", {
+		...props.modelValue,
+		images
+	})
+}
+
+const toggleImageThumbnail = (image_id) => {
+	let thumbnail_id = props.modelValue.thumbnail_id === image_id ? null : image_id
+
+	emit("update:modelValue", Object.assign(props.modelValue, { thumbnail_id }))
+}
+
+// todo rejection handling
+const imageRejected = (rejected_entries) => {
+	console.log(rejected_entries)
+}
+
+// todo - bind uploading to this method
+const upload = (files) => {
+
+}
+
+const drop = (e) => {
+	is_dragging.value = false
+	uploader.value.addFiles([e.dataTransfer.files[0]])
+}
+
 </script>
