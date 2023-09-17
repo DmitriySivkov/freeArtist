18af7aa8486421a181345194efe6095e9886a32b
Dmitrii Sivkov <sivkovdw@yandex.ru>
2023-09-17 22:08:06 +0300

    cart summary

--- quasar/src/components/cart/OrderSummary.vue
+++ quasar/src/components/cart/OrderSummary.vue
@@ -1,11 +1,12 @@
 <template>
-	<q-card class="full-height">
-		<q-card-section class="text-center">
-			Стоимость: {{ total_price }} р.
+	<q-card>
+		<q-card-section class="text-center text-body1">
+			Стоимость: {{ totalPrice }} ₽
 		</q-card-section>
 		<q-separator />
 		<q-card-section class="text-center">
 			<q-btn
+				class="q-pa-md"
 				label="Оформить заказ"
 				color="primary"
 				@click="makeNewOrder"
@@ -14,47 +15,38 @@
 	</q-card>
 </template>
 <!-- todo - redirect to user orders page on new order -->
-<script>
+<script setup>
 import { computed } from "vue"
 import { useCartStore } from "src/stores/cart"
 import { useOrderStore } from "src/stores/order"
 import { useNotification } from "src/composables/notification"
 import { Loading } from "quasar"
-import { useCart } from "src/composables/cart"
-export default {
-	setup() {
-		const cart_store = useCartStore()
-		const order_store = useOrderStore()
-		const { notifySuccess } = useNotification()
-		const { clearCart } = useCart()
 
-		const cart = computed(() => cart_store.data)
+const cartStore = useCartStore()
+const order_store = useOrderStore()
+const { notifySuccess } = useNotification()
 
-		const total_price = computed(() => {
-			return Object.values(cart.value).reduce(
-				(accum, cart_item) =>
-					accum + cart_item.product_list.reduce((ac, product) => ac + product.data.price * product.amount, 0),
-				0).toFixed(2)
-		})
+const cart = computed(() => cartStore.data)
 
-		const makeNewOrder = () => {
-			Loading.show({ spinnerColor: "primary" })
+const totalPrice = computed(() =>
+	cart.value.reduce(
+		(carry, producerSet) =>
+			carry + producerSet.products.reduce((c, productSet) => c + productSet.data.price * productSet.cart_amount, 0),
+		0).toFixed(2)
+)
 
-			const promise = order_store.create(cart.value)
+const makeNewOrder = () => {
+	Loading.show({ spinnerColor: "primary" })
 
-			promise.then(() => {
-				clearCart()
+	const promise = order_store.create(cart.value)
 
-				Loading.hide()
+	promise.then(() => {
+		clearCart()
 
-				notifySuccess("Заказ успешно оформлен")
-			})
-		}
+		Loading.hide()
 
-		return {
-			total_price,
-			makeNewOrder
-		}
-	}
+		notifySuccess("Заказ успешно оформлен")
+	})
 }
+
 </script>
--- quasar/src/pages/Cart.vue
+++ quasar/src/pages/Cart.vue
@@ -3,7 +3,9 @@
 		<div class="row justify-center fit">
 			<div class="col-xs-12 col-md-8 col-lg-7 col-xl-6 bg-white">
 				<CartProductList />
-				<!--					<OrderSummary v-if="cart.length > 0" />-->
+				<div class="sticky__common_bottom">
+					<OrderSummary />
+				</div>
 			</div>
 		</div>
 	</div>
@@ -13,7 +15,7 @@
 import CartProductList from "src/components/cart/CartProductList.vue"
 import OrderSummary from "src/components/cart/OrderSummary.vue"
 import { useCartStore } from "src/stores/cart"
-import { computed, onMounted, ref } from "vue"
+import { computed } from "vue"
 
 const cartStore = useCartStore()
 
