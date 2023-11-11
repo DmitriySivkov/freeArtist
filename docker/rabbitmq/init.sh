55eb08b46f33841904c0a8bc1d71609af6626299
samokeev <s.makeev@alef.im>
2023-11-11 17:32:56 +0500

    Поправил опечатку

--- docker/rabbitmq/init.sh
+++ docker/rabbitmq/init.sh
@@ -2,7 +2,7 @@
 
 echo "Rabbit init script";
 ( rabbitmqctl wait --timeout 10 /var/lib/rabbitmq/mnesia/rabbitmq ; \
-rabbitmqadmin --username root --password root declare queue name=Broadcast ; \
+rabbitmqadmin --username root --password root declare queue name=broadcast ; \
 echo "Rabbit init fired"; ) &
 
 rabbitmq-server $@
