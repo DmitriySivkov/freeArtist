1464c0d7c08a449f4d2689f71d817e500b6e0a56
Dmitrii Sivkov <sivkovdw@yandex.ru>
2023-11-16 00:32:11 +0300

    producer personal image tab - start reworking

--- quasar/src/components/producers/producerProductSettingsTabs/ProducerProductSettingImagesTab.vue
+++ quasar/src/components/producers/producerProductSettingsTabs/ProducerProductSettingImagesTab.vue
@@ -1,8 +1,11 @@
 <template>
 	<q-card
 		bordered
-		class="col-xs-12 col-md-4 border-dashed bg-green-3 shadow-0 q-mb-md"
-		:class="{'text-white bg-green-6 border-white': is_dragging, 'border-black': !is_dragging}"
+		class="col-xs-12 col-md-4 border-dashed bg-green-3 shadow-0 q-mb-xs"
+		:class="[
+			{'text-white bg-green-6 border-white': is_dragging},
+			{'border-black': !is_dragging}
+		]"
 		style="height:200px"
 	>
 		<q-card-section class="row flex-center full-height">
@@ -57,66 +60,125 @@
 		</q-card>
 	</div>
 
-	<q-separator
-		v-if="modelValue.committed_images && modelValue.images"
-		class="q-my-md bg-primary"
-	/>
-
-	<div class="row q-col-gutter-sm">
-		<q-card
-			flat
-			class="col-xs-12 col-md-6"
-			v-for="image in modelValue.images"
-			:key="image.id"
-		>
-			<q-card-section
-				horizontal
-				class="bg-indigo-7 q-mb-xs q-pa-xs"
+	<q-carousel
+		ref="carousel"
+		v-model="slide"
+		class="bg-secondary"
+		transition-prev="fade"
+		transition-next="fade"
+		control-type="regular"
+		control-color="primary"
+		control-text-color="white"
+		swipeable
+		animated
+		height="auto"
+		:arrows="modelValue.images.length > 2"
+	>
+		<template v-if="isWidthThreshold">
+			<q-carousel-slide
+				v-for="n in Math.ceil(modelValue.images.length/2)"
+				:key="n"
+				:name="n"
+				class="column no-wrap q-px-none q-py-sm"
 			>
-				<div class="col text-left">
-					<q-icon
-						class="cursor-pointer"
-						size="32px"
-						name="wallpaper"
-						:color="modelValue.thumbnail_id === image.id ? 'white' : 'black'"
-						@click="toggleImageThumbnail(image.id)"
-					/>
+				<div
+					v-if="isWidthThreshold"
+					class="row fit flex-center"
+				>
+					<div class="col-sm-8 col-lg-6">
+						<div class="row fit flex-center q-gutter-xs q-col-gutter no-wrap">
+							<q-img
+								v-for="image in modelValue.images.slice((n-1)*2, n*2)"
+								:key="image.id"
+								no-spinner
+								class="rounded-borders col-6 full-height"
+								fit="cover"
+								:src="`${backendServer}/storage/${image.path}`"
+								:ratio="16/9"
+							/>
+						</div>
+					</div>
 				</div>
-				<div class="col self-end text-right">
-					<q-icon
-						class="cursor-pointer"
-						size="32px"
-						:name="!image.to_delete ? 'clear' : 'restore'"
-						:color="!image.to_delete ? 'black' : 'white'"
-						@click="toggleImageRemoval(image.id)"
-					/>
-				</div>
-			</q-card-section>
-			<q-card-section horizontal>
+			</q-carousel-slide>
+		</template>
+		<template v-else>
+			<!--			<q-carousel-slide-->
+			<!--				v-for="(image, i) in modelValue.images"-->
+			<!--				:key="image.id"-->
+			<!--				:name="i+1"-->
+			<!--				:img-src="`${backendServer}/storage/${image.path}`"-->
+			<!--			/>-->
+			<q-carousel-slide
+				v-for="(image, i) in modelValue.images"
+				:key="image.id"
+				:name="i+1"
+				class="q-pa-xs"
+			>
 				<q-img
-					:src="backend_server + '/storage/' + image.path"
 					no-spinner
-					fit="contain"
-					style="height: 200px;"
+					class="rounded-borders"
+					fit="cover"
+					:src="`${backendServer}/storage/${image.path}`"
+					:ratio="16/9"
 				/>
-			</q-card-section>
-		</q-card>
-	</div>
+			</q-carousel-slide>
+		</template>
 
-	<q-uploader
-		ref="uploader"
-		accept=".jpg,image/*"
-		multiple
+	</q-carousel>
+
+	<!--	<div class="row q-col-gutter-sm">-->
+	<!--		<q-card-->
+	<!--			flat-->
+	<!--			class="col-xs-12 col-md-6"-->
+	<!--			v-for="image in modelValue.images"-->
+	<!--			:key="image.id"-->
+	<!--		>-->
+	<!--			<q-card-section-->
+	<!--				horizontal-->
+	<!--				class="bg-indigo-7 q-mb-xs q-pa-xs"-->
+	<!--			>-->
+	<!--				<div class="col text-left">-->
+	<!--					<q-icon-->
+	<!--						class="cursor-pointer"-->
+	<!--						size="32px"-->
+	<!--						name="wallpaper"-->
+	<!--						:color="modelValue.thumbnail_id === image.id ? 'white' : 'black'"-->
+	<!--						@click="toggleImageThumbnail(image.id)"-->
+	<!--					/>-->
+	<!--				</div>-->
+	<!--				<div class="col self-end text-right">-->
+	<!--					<q-icon-->
+	<!--						class="cursor-pointer"-->
+	<!--						size="32px"-->
+	<!--						:name="!image.to_delete ? 'clear' : 'restore'"-->
+	<!--						:color="!image.to_delete ? 'black' : 'white'"-->
+	<!--						@click="toggleImageRemoval(image.id)"-->
+	<!--					/>-->
+	<!--				</div>-->
+	<!--			</q-card-section>-->
+	<!--			<q-card-section horizontal>-->
+	<!--				<q-img-->
+	<!--					:src="backendServer + '/storage/' + image.path"-->
+	<!--					no-spinner-->
+	<!--					fit="contain"-->
+	<!--					style="height: 200px;"-->
+	<!--				/>-->
+	<!--			</q-card-section>-->
+	<!--		</q-card>-->
+	<!--	</div>-->
+
+	<q-file
+		v-model="image"
+		ref="file_picker"
+		accept=".jpg, image/*"
 		style="display:none"
-		:factory="upload"
-		@added="imageCommitted"
-		@rejected="imageRejected"
+		@update:model-value="addImage"
 	/>
 </template>
 
 <script setup>
 import { useQuasar } from "quasar"
-import { ref } from "vue"
+import {computed, ref} from "vue"
 import { Plugins, CameraResultType } from "@capacitor/core"
 import { cameraService } from "src/services/cameraService"
 import AddImageDialog from "src/components/dialogs/AddImageDialog.vue"
@@ -136,6 +198,8 @@ const emit = defineEmits([
 const $q = useQuasar()
 const image = ref(null)
 
+const slide = ref(1)
+
 const uploader = ref(null)
 const is_dragging = ref(false)
 const is_loading = ref(false)
@@ -143,7 +207,9 @@ const is_loading = ref(false)
 const { base64ToBlob } = cameraService()
 const { Camera } = Plugins
 
-const backend_server = process.env.BACKEND_SERVER
+const backendServer = process.env.BACKEND_SERVER
+
+const isWidthThreshold = computed(() => $q.screen.width >= $q.screen.sizes.sm)
 
 const showFilePrompt = () => {
 	if ($q.platform.is.desktop) {
@@ -195,7 +261,6 @@ const imageCommitted = (files) => {
 		...props.modelValue,
 		committed_images: [...tmp_images, ...props.modelValue.committed_images]
 	})
-
 }
 
 const removeCommittedImage = (committed_image_key) => {
@@ -248,16 +313,6 @@ const toggleImageThumbnail = (image_id) => {
 	let thumbnail_id = props.modelValue.thumbnail_id === image_id ? null : image_id
 
 	emit("update:modelValue", Object.assign(props.modelValue, { thumbnail_id }))
-}
-
-// todo rejection handling
-const imageRejected = (rejected_entries) => {
-	console.log(rejected_entries)
-}
-
-// todo - bind uploading to this method
-const upload = (files) => {
-
 }
 
 const drop = (e) => {
