5832b2e900b5dabf237d0603ede7e6790f593c60
Dmitrii Sivkov <sivkovdw@yandex.ru>
2024-09-06 01:46:30 +0300

    docker

--- docker.sh
+++ docker.sh
@@ -3,5 +3,5 @@ if [ "$1" == "local" ]
 then
 		docker-compose -f docker-compose.local.yml up
 else
-    docker-compose -f docker-compose.production.yml up -d
+    docker-compose -f docker-compose.production.yml up
 fi
