33ddb976dc767b5978a71dccb9358dd5a5bccf9d
Dmitrii Sivkov <sivkovdw@yandex.ru>
2024-09-09 23:41:23 +0300

    more work to quasar dockerization

--- docker.sh
+++ docker.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 if [ "$1" == "local" ]
 then
+		echo -e '\033[0;31m ---------- LAUNCHING THE SPACE SHUTTLE UP ---------- \e[0m'
+
+		# копируем node_modules из контейнера на хост (для дебага, например)
+		./copy-node-modules.sh &
+
 		docker-compose -f docker-compose.local.yml up
 else
     docker-compose -f docker-compose.production.yml up -d
