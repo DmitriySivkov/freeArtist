0585cf4ccbb318a9c9ea1981c40419780d14293f
Dmitrii Sivkov <sivkovdw@yandex.ru>
2024-11-02 21:59:47 +0300

    added producer_order_id to orders table

--- main/database/migrations/2022_01_23_170616_create_orders_table.php
+++ main/database/migrations/2022_01_23_170616_create_orders_table.php
@@ -19,8 +19,8 @@ class CreateOrdersTable extends Migration
             $table->unsignedInteger('user_id')
 				->nullable();
             $table->unsignedInteger('producer_id');
-			$table->unsignedInteger('assignee_id')
-				->nullable();
+			$table->unsignedInteger('producer_order_id');
+			$table->unsignedInteger('assignee_id');
             $table->json('order_products');
             $table->integer('status');
 			$table->date('prepare_by');
